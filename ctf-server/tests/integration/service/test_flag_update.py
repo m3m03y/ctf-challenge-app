10e99dcd266f6741b37391f0d7fb04ab3f73525e
memoey <kinga@lipisz.co>
2024-03-18 22:22:47 +0100

    tests: flag updates

---
+++ ctf-server/tests/integration/service/test_flag_update.py
@@ -0,0 +1,81 @@
+"""Test flag management service module - update flag part"""
+
+from ctf_server.core.crypto import Crypto
+from ctf_server.model.flag import Flag
+from ctf_server.core.flag_validator_strategy import PlainInputStoredHashedStrategy
+from ctf_server.db.mongodb_proxy import MongodbProxy
+from ctf_server.service.flag_service import FlagService
+
+
+class TestFlagUpdate:
+    """Tests flag service update functions"""
+
+    def test_valid_flag_should_be_updated_and_contain_new_hashed_value(
+        self, flag_collection_with_data, connection_url
+    ):
+        """Test flag update"""
+        initial_size = len(list(flag_collection_with_data.find()))
+        flag_service = FlagService(
+            MongodbProxy(connection_url),
+            PlainInputStoredHashedStrategy(),
+        )
+
+        flag = Flag(
+            value="flag{test_updated_name}",
+            task_id="firsttask",
+            challange_id="firstchallange",
+        )
+        updated_flag = flag_service.update_flag(flag)
+        assert updated_flag.challange_id == flag.challange_id
+        assert updated_flag.task_id == flag.task_id
+        assert updated_flag.value == Crypto.hash_to_md5(flag.value)
+
+        updated_size = len(list(flag_collection_with_data.find()))
+        assert initial_size == updated_size
+
+    def test_invalid_flag_should_not_overwrite_db_data(
+        self, flag_collection_with_data, connection_url
+    ):
+        """Test flag update"""
+        initial_size = len(list(flag_collection_with_data.find()))
+        flag_service = FlagService(
+            MongodbProxy(connection_url),
+            PlainInputStoredHashedStrategy(),
+        )
+
+        flag = Flag(
+            value="flag{INVALID-NAME}",
+            task_id="firsttask",
+            challange_id="firstchallange",
+        )
+        updated_flag = flag_service.update_flag(flag)
+        assert updated_flag is None
+
+        updated_size = len(list(flag_collection_with_data.find()))
+        assert initial_size == updated_size
+
+        existing_record = flag_collection_with_data.find(
+            {"challange_id": flag.challange_id, "task_id": flag.task_id}
+        )[0]
+        assert existing_record["value"] == Crypto.hash_to_md5("flag{test_1}")
+
+    def test_update_not_existing_flag_should_fail(
+        self, flag_collection_with_data, connection_url
+    ):
+        """Test flag update"""
+        initial_size = len(list(flag_collection_with_data.find()))
+        flag_service = FlagService(
+            MongodbProxy(connection_url),
+            PlainInputStoredHashedStrategy(),
+        )
+
+        flag = Flag(
+            value="flag{test_update}",
+            task_id="secondtask",
+            challange_id="secondchallange",
+        )
+        updated_flag = flag_service.update_flag(flag)
+        assert updated_flag is None
+
+        updated_size = len(list(flag_collection_with_data.find()))
+        assert initial_size == updated_size
