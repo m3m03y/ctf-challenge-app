1c5b23319adb9b6fc3d2b2549d470f1a16703af2
memoey <kinga@lipisz.co>
2024-04-08 22:09:48 +0200

    feat: working function api to submit flag

--- ctf-server/function_app.py
+++ ctf-server/function_app.py
@@ -2,6 +2,8 @@
 
 import logging
 import azure.functions as func
+from ctf_server import config
+from ctf_server.core import crypto
 from ctf_server.core.flag_validator_strategy import PlainInputStoredHashedStrategy
 from ctf_server.db.azure_proxy import AzureProxy
 from ctf_server.model.flag import Flag
@@ -11,7 +13,9 @@ from ctf_server.service.flag_service import FlagService
 app = func.FunctionApp()
 
 
-@app.route(route="submit", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.POST])
+@app.route(
+    route="submit", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.POST]
+)
 def submit(req: func.HttpRequest) -> func.HttpResponse:
     """Based on user input request to check whether  flag is valid"""
     logging.info("Python HTTP trigger function processed a request.")
@@ -34,3 +38,13 @@ def submit(req: func.HttpRequest) -> func.HttpResponse:
         State.INVALID_FLAG,
         status_code=200,
     )
+
+
+@app.route(
+    route="health", auth_level=func.AuthLevel.ANONYMOUS, methods=[func.HttpMethod.GET]
+)
+def health(req: func.HttpRequest) -> func.HttpResponse:
+    """Function to validate whether Function is running"""
+    logging.info("Python HTTP trigger function processed a request.")
+
+    return func.HttpResponse("Function is healthy", status_code=200)
