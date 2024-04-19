c4022bf8b0342ce0092785d40b843536f4e88488
memoey <kinga@lipisz.co>
2024-04-19 12:02:04 +0200

    style: format flag routes module

--- ctf-server/ctf_server/api/flag_routes.py
+++ ctf-server/ctf_server/api/flag_routes.py
@@ -11,6 +11,7 @@ from ctf_server.service.flag_service import FlagService
 router = fastapi.APIRouter()
 flag_service = FlagService(AzureProxy(), PlainInputStoredHashedStrategy())
 
+
 @router.post("/submit-flag", status_code=status.HTTP_200_OK)
 async def submit_flag(flag: Flag, response: Response) -> dict:
     """Handles flag submit"""
@@ -18,9 +19,10 @@ async def submit_flag(flag: Flag, response: Response) -> dict:
     if state in (State.INVALID_FORMAT, State.INVALID_FLAG):
         response.status_code = status.HTTP_400_BAD_REQUEST
         return {"stage": state}
-    
+
     next_task = flag_service.get_next_task(flag.challenge_id, flag.task_nr)
     return {"state": state, "task_nr": next_task}
+
 
 @router.post("/flag", status_code=status.HTTP_201_CREATED)
 async def create_flag(flag: Flag, response: Response) -> dict:
@@ -29,7 +31,8 @@ async def create_flag(flag: Flag, response: Response) -> dict:
     flag = flag_service.create_flag(flag)
     if flag is None:
         response.status_code = status.HTTP_400_BAD_REQUEST
-    return {"error": "CREATE_FAILED" } if flag is None else {"flag": flag}
+    return {"error": "CREATE_FAILED"} if flag is None else {"flag": flag}
+
 
 @router.get("/flag/", status_code=status.HTTP_200_OK)
 async def get_flag(challenge_id: str, task_id: str) -> dict:
@@ -37,17 +40,20 @@ async def get_flag(challenge_id: str, task_id: str) -> dict:
     flag = flag_service.get_flag(challenge_id, task_id)
     return {"flag": flag}
 
+
 @router.get("/flag", status_code=status.HTTP_200_OK)
 async def get_all_flags() -> dict:
     """Handles flag submit"""
     flags = flag_service.get_all_flags()
     return {"flags": flags}
 
+
 @router.put("/flag", status_code=status.HTTP_200_OK)
 async def update_flag(flag: Flag) -> dict:
     """Handles flag update"""
     flag = flag_service.update_flag(flag)
-    return {"error": "UPDATE_FAILED" } if flag is None else {"flag": flag}
+    return {"error": "UPDATE_FAILED"} if flag is None else {"flag": flag}
+
 
 @router.delete("/flag/", status_code=status.HTTP_200_OK)
 async def delete_flag(challenge_id: str, task_id: str) -> dict:
